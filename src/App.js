11ca0d6bd1a5c1f95253ce994a9fbd41a16e8075
Zack McKenna <zackmckenna@gmail.com>
2020-04-26 15:53:09 -0400

    added compass to webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import compass from './compass.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={compass} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PIRATES OF THE DELAWARE
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>coming soon</p>
       </header>
     </div>
   );
